72ac6114ae2e2f027c994214631d3a0758e1ad29
Ott Toomet <otoomet@uw.edu>
2018-01-24 15:07:44 -0800

    exercise-3 explained better

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,43 +1,37 @@
-# Exercise 3: Data Frame Practice with `dplyr`.
-# Use a different appraoch to accomplish the same tasks as exercise-1
+### Exercise 3: Data Frame Practice with dplyr with and without pipes
 
+## This exercise need the "fueleconomy" package.  Install and load it.
+## You should have have access to the `vehicles` data.frame
+## View(vehicles) # rstudio only
 
-# Install devtools package: allows installations from GitHub
-install.packages('devtools')
-
-# Install "fueleconomy" package from GitHub
-devtools::install_github("hadley/fueleconomy")
-
-# Require/library the fueleconomy package
-library(fueleconomy)
-
-# Install (if you haven't already) and load the `dplyr`package
+## Install (if you haven't already) and load the `dplyr`package
 library(dplyr)
 
-# You should have have access to the vehicles data.frame
-View(vehicles)
-
-# Which Accura model has the best hwy MPG in 2015? (without method chaining)
+## Which Accura model has the best hwy MPG in 2015?
+## use temporary variables.
 acuras <- filter(vehicles, make == 'Acura', year == 2015)
 best.acura <- filter(acuras, hwy == max(hwy))
 best.model <- select(best.acura, model)
 
-# Which Accura model has the best hwy MPG in 2015? (nesting functions)
+## Which Accura model has the best hwy MPG in 2015?
+## use nested functions
 best.model <- select(
   filter(
     filter(vehicles, make == 'Acura', year == 2015), hwy == max(hwy)
   ), model
 )
 
-# Which Accura model has the best hwy MPG in 2015? (pipe operator)
+## Which Accura model has the best hwy MPG in 2015?
+## use pipes
 best.model <- filter(vehicles, make == 'Acura', year == 2015) %>%
   filter(hwy == max(hwy)) %>%
   select(model)
 
 ### Bonus ###
 
-# Write 3 functions, one for each approach.  Then, 
-# Test how long it takes to perform each one 1000 times
+## Write 3 functions, one for each approach.  Then, 
+## Test how long it takes to perform each one 1000 times
+## Note: you may instead try the 'microbenchmark' library
 
 # Without chaining
 WithoutChaining <- function() {
@@ -62,7 +56,7 @@ PipeBestModel <- function() {
     select(model)
 }
 
-# Pretty similar...
+# Results are pretty similar...
 system.time(for (i in 1:1000) WithoutChaining())
 system.time(for (i in 1:1000) NestedBestModel())
 system.time(for (i in 1:1000) PipeBestModel())
